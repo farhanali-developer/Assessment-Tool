17f87e6416ecc6a3488fd634f33537a2d31e5ade
Farhan Ali <fareeboy12@gmail.com>
2021-11-15 04:13:10 +0500

    Nothing changed in this version.

--- admin/class-assessment_tool-admin.php
+++ admin/class-assessment_tool-admin.php
@@ -100,8 +100,10 @@ class Assessment_tool_Admin {
 		 * class.
 		 */
 
+		wp_enqueue_script( "jquery", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js", array( 'jquery' ), true );
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/assessment_tool-admin.js', array( 'jquery' ), $this->version, true );
 		wp_enqueue_script( "bootstrap", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js", array( 'jquery' ), $this->version, true );
+		wp_enqueue_script( "repeater", "https://cdnjs.cloudflare.com/ajax/libs/jquery.repeater/1.2.1/jquery.repeater.min.js", array( 'jquery' ), $this->version, true );
 
 	}
 
@@ -124,22 +126,185 @@ function assessment_form_function(){
 	<div class="container-fluid mt-5">
 		<div class="row mx-auto justify-content-between">
 			<div class="col-12 col-md-5 offset-md-2">
-				<form>
-					<div class="mb-3">
-						<label for="exampleInputEmail1" class="form-label">Email address</label>
-						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-						<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-					</div>
-					<div class="mb-3">
-						<label for="exampleInputPassword1" class="form-label">Password</label>
-						<input type="password" class="form-control" id="exampleInputPassword1">
-					</div>
-					<div class="mb-3 form-check">
-						<input type="checkbox" class="form-check-input" id="exampleCheck1">
-						<label class="form-check-label" for="exampleCheck1">Check me out</label>
-					</div>
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</form>
+
+
+
+
+
+			<div class="repeater-default">
+                            <div data-repeater-list="car">
+                                <div data-repeater-item="">
+                                    <form class="form repeater row AVAST_PAM_loginform" data-nordpass-autofill="login" data-np-checked="1" data-nordpass-watching="1">
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="email-addr">Email address</label>
+                                            <br>
+                                            <input type="email" class="form-control" id="email-addr" placeholder="Enter email" data-np-checked="1" data-nordpass-autofill="username" data-nordpass-uid="zpk92xes7zi" autocomplete="off">
+                                        <span data-nordpass-uid="zpk92xes7zi" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="pass">Password</label>
+                                            <br>
+                                            <input type="password" class="form-control" id="pass" placeholder="Password" data-np-checked="1" data-nordpass-autofill="password" data-nordpass-uid="6g6e44j1obk" autocomplete="off">
+                                        <span data-nordpass-uid="6g6e44j1obk" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="bio" class="cursor-pointer">Bio</label>
+                                            <br>
+                                            <textarea class="form-control" id="bio" rows="2"></textarea>
+                                        </div>
+                                        <div class="skin skin-flat form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="tel-input">Gender</label>
+                                            <br>
+                                            <input class="form-control" type="tel" value="1-(555)-555-5555" id="tel-input" data-np-checked="1">
+                                        </div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="profession">Profession</label>
+                                            <br>
+                                            <select class="form-control" id="profession">
+                                              <option>Select Option</option>
+                                              <option>Option 1</option>
+                                              <option>Option 2</option>
+                                              <option>Option 3</option>
+                                              <option>Option 4</option>
+                                              <option>Option 5</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-sm-12 col-md-2 text-center mt-2">
+                                            <button type="button" class="btn btn-danger" data-repeater-delete=""> <i class="feather icon-x"></i> Delete</button>
+                                        </div>
+                                    </form>
+                                    <hr>
+                                </div>
+                            <div data-repeater-item="" style="">
+                                    <form class="form row AVAST_PAM_loginform" data-nordpass-autofill="identity" data-np-checked="1" data-nordpass-watching="1">
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="email-addr">Email address</label>
+                                            <br>
+                                            <input type="email" class="form-control" id="email-addr" placeholder="Enter email" data-np-checked="1" data-nordpass-autofill="identity_email" data-nordpass-uid="nhqa3fj5gzs" autocomplete="off">
+                                        <span data-nordpass-uid="nhqa3fj5gzs" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="pass">Password</label>
+                                            <br>
+                                            <input type="password" class="form-control" id="pass" placeholder="Password" data-np-checked="1">
+                                        </div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="bio" class="cursor-pointer">Bio</label>
+                                            <br>
+                                            <textarea class="form-control" id="bio" rows="2"></textarea>
+                                        </div>
+                                        <div class="skin skin-flat form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="tel-input">Gender</label>
+                                            <br>
+                                            <input class="form-control" type="tel" value="1-(555)-555-5555" id="tel-input" data-np-checked="1" data-nordpass-autofill="identity_phone_number" data-nordpass-uid="ihefqe7iat" autocomplete="off">
+                                        <span data-nordpass-uid="ihefqe7iat" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="profession">Profession</label>
+                                            <br>
+                                            <select class="form-control" id="profession">
+                                              <option>Select Option</option>
+                                              <option>Option 1</option>
+                                              <option>Option 2</option>
+                                              <option>Option 3</option>
+                                              <option>Option 4</option>
+                                              <option>Option 5</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-sm-12 col-md-2 text-center mt-2">
+                                            <button type="button" class="btn btn-danger" data-repeater-delete=""> <i class="feather icon-x"></i> Delete</button>
+                                        </div>
+                                    </form>
+                                    <hr>
+                                </div><div data-repeater-item="" style="">
+                                    <form class="form row AVAST_PAM_loginform" data-nordpass-autofill="identity" data-np-checked="1" data-nordpass-watching="1">
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="email-addr">Email address</label>
+                                            <br>
+                                            <input type="email" class="form-control" id="email-addr" placeholder="Enter email" data-np-checked="1" data-nordpass-autofill="identity_email" data-nordpass-uid="buhwjqv71xu" autocomplete="off">
+                                        <span data-nordpass-uid="buhwjqv71xu" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="pass">Password</label>
+                                            <br>
+                                            <input type="password" class="form-control" id="pass" placeholder="Password" data-np-checked="1">
+                                        </div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="bio" class="cursor-pointer">Bio</label>
+                                            <br>
+                                            <textarea class="form-control" id="bio" rows="2"></textarea>
+                                        </div>
+                                        <div class="skin skin-flat form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="tel-input">Gender</label>
+                                            <br>
+                                            <input class="form-control" type="tel" value="1-(555)-555-5555" id="tel-input" data-np-checked="1" data-nordpass-autofill="identity_phone_number" data-nordpass-uid="p9pngrfqz6" autocomplete="off">
+                                        <span data-nordpass-uid="p9pngrfqz6" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="profession">Profession</label>
+                                            <br>
+                                            <select class="form-control" id="profession">
+                                              <option>Select Option</option>
+                                              <option>Option 1</option>
+                                              <option>Option 2</option>
+                                              <option>Option 3</option>
+                                              <option>Option 4</option>
+                                              <option>Option 5</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-sm-12 col-md-2 text-center mt-2">
+                                            <button type="button" class="btn btn-danger" data-repeater-delete=""> <i class="feather icon-x"></i> Delete</button>
+                                        </div>
+                                    </form>
+                                    <hr>
+                                </div><div data-repeater-item="" style="">
+                                    <form class="form row AVAST_PAM_loginform" data-nordpass-autofill="identity" data-np-checked="1" data-nordpass-watching="1">
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="email-addr">Email address</label>
+                                            <br>
+                                            <input type="email" class="form-control" id="email-addr" placeholder="Enter email" data-np-checked="1" data-nordpass-autofill="identity_email" data-nordpass-uid="8b4o8rhf39x" autocomplete="off">
+                                        <span data-nordpass-uid="8b4o8rhf39x" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="pass">Password</label>
+                                            <br>
+                                            <input type="password" class="form-control" id="pass" placeholder="Password" data-np-checked="1">
+                                        </div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="bio" class="cursor-pointer">Bio</label>
+                                            <br>
+                                            <textarea class="form-control" id="bio" rows="2"></textarea>
+                                        </div>
+                                        <div class="skin skin-flat form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="tel-input">Gender</label>
+                                            <br>
+                                            <input class="form-control" type="tel" value="1-(555)-555-5555" id="tel-input" data-np-checked="1" data-nordpass-autofill="identity_phone_number" data-nordpass-uid="ctwzeu324o" autocomplete="off">
+                                        <span data-nordpass-uid="ctwzeu324o" style="width: 24px; min-width: 24px; height: 24px; background-image: url(&quot;chrome-extension://fooolghllnmhmmndgjiamiiodkpenpbb/icons/icon.svg&quot;); background-repeat: no-repeat; background-position: left center; background-size: auto; border: none; display: inline; visibility: visible; position: absolute; cursor: pointer; z-index: 1001; padding: 0px; transition: none 0s ease 0s; pointer-events: all; left: 203.766px; top: 34.5px;"></span></div>
+                                        <div class="form-group mb-1 col-sm-12 col-md-2">
+                                            <label for="profession">Profession</label>
+                                            <br>
+                                            <select class="form-control" id="profession">
+                                              <option>Select Option</option>
+                                              <option>Option 1</option>
+                                              <option>Option 2</option>
+                                              <option>Option 3</option>
+                                              <option>Option 4</option>
+                                              <option>Option 5</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-sm-12 col-md-2 text-center mt-2">
+                                            <button type="button" class="btn btn-danger" data-repeater-delete=""> <i class="feather icon-x"></i> Delete</button>
+                                        </div>
+                                    </form>
+                                    <hr>
+                                </div></div>
+                            <div class="form-group overflow-hidden">
+                                <div class="col-12">
+                                    <button data-repeater-create="" class="btn btn-primary btn-lg">
+                                        <i class="icon-plus4"></i> Add
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
 			</div>
 			<div class="col-12 col-md-3">	
 				<div class="card text-dark bg-light mb-3 p-0">
